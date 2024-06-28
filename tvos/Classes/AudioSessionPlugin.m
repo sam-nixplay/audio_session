4db79d9007e003aa7f33b1181809f9514264bcef
Sam Smets <sam.smets@nix-digital.com>
2024-06-28 15:09:48 +0800

    Fixed audio session impl

--- tvos/Classes/AudioSessionPlugin.m
+++ tvos/Classes/AudioSessionPlugin.m
@@ -1,5 +1,5 @@
 #import "AudioSessionPlugin.h"
-#if !TARGET_OS_TV
+#if TARGET_OS_IOS
 #import "DarwinAudioSession.h"
 #endif
 
@@ -7,10 +7,8 @@ static NSObject *configuration = nil;
 static NSHashTable<AudioSessionPlugin *> *plugins = nil;
 
 @implementation AudioSessionPlugin {
-#if !TARGET_OS_TV
+#if TARGET_OS_IOS
     DarwinAudioSession *_darwinAudioSession;
-#else
-    // Add any tvOS-specific properties here if needed
 #endif
     FlutterMethodChannel *_channel;
 }
@@ -31,14 +29,9 @@ static NSHashTable<AudioSessionPlugin *> *plugins = nil;
               binaryMessenger:[registrar messenger]];
     [registrar addMethodCallDelegate:self channel:_channel];
 
-#if !TARGET_OS_TV
+#if TARGET_OS_IOS
     _darwinAudioSession = [[DarwinAudioSession alloc] initWithRegistrar:registrar];
-#else
-    // Set up any tvOS-specific audio session handling here
-    // For example, you might want to configure basic audio session settings
-    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
 #endif
-
     return self;
 }
 
@@ -57,37 +50,8 @@ static NSHashTable<AudioSessionPlugin *> *plugins = nil;
     } else if ([@"getConfiguration" isEqualToString:call.method]) {
         result(configuration);
     } else {
-#if !TARGET_OS_TV
-        if ([@"someMethodSpecificToDarwinAudioSession" isEqualToString:call.method]) {
-            // Call the specific method on DarwinAudioSession
-            [_darwinAudioSession someMethodSpecificToDarwinAudioSession:args result:result];
-        } else {
-            // Forward other method calls to DarwinAudioSession
-            [_darwinAudioSession handleMethodCall:call result:result];
-        }
-#else
-        // Handle tvOS-specific audio session methods here
-        if ([@"getTvOsAudioSessionInfo" isEqualToString:call.method]) {
-            // Example of a tvOS-specific method
-            [self getTvOsAudioSessionInfo:result];
-        } else {
-            result(FlutterMethodNotImplemented);
-        }
-#endif
+        result(FlutterMethodNotImplemented);
     }
 }
-
-#if TARGET_OS_TV
-// Example of a tvOS-specific method
-- (void)getTvOsAudioSessionInfo:(FlutterResult)result {
-    AVAudioSession *session = [AVAudioSession sharedInstance];
-    NSDictionary *info = @{
-        @"category": session.category,
-        @"sampleRate": @(session.sampleRate),
-        @"outputLatency": @(session.outputLatency)
-    };
-    result(info);
-}
-#endif
 
 @end
